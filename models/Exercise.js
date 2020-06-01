9d7a7c4e130aba8ccb40ea578d3575eeaea67f4a
Drake <drakedemuyt@gmail.com>
2020-06-01 01:44:15 -0400

    Initial commit

---
+++ models/Exercise.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type:{
+
+    },
+    name: {
+
+    },
+    duration: {
+
+    },
+    weight: {
+
+    },
+    reps: {
+
+    },
+    sets: {
+        
+    }
+});
+
+module.exports = Workout;
